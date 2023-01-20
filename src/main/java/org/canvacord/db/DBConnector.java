36dea2ada5e911f87662712dc4a05df3c5f5f89d
Garrett <gnixsoftware@gmail.com>
2023-01-19 16:13:25 -0800

    Comment to warn about the DB that no longer works

--- src/main/java/org/canvacord/db/DBConnector.java
+++ src/main/java/org/canvacord/db/DBConnector.java
@@ -8,6 +8,7 @@ public class DBConnector {
 
 	public static Optional<Connection> connectToDB() {
 		try {
+			// TODO replace the connection string with a working DB again
 			Connection connection = DriverManager.getConnection("jdbc:mysql://sql9.freemysqlhosting.net/sql9582292", "sql9582292", "IuATrw88Z1");
 			return Optional.of(connection);
 		}
