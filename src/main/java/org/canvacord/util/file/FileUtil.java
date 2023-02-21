cb16b7fb34f39db63d115c472f7cf0a7f39854e1
Garrett <gnixsoftware@gmail.com>
2023-02-21 15:58:53 -0800

    Added Javadoc for new FileUtil methods

--- src/main/java/org/canvacord/util/file/FileUtil.java
+++ src/main/java/org/canvacord/util/file/FileUtil.java
@@ -174,6 +174,12 @@ public class FileUtil {
 		}
 	}
 
+	/**
+	 * Validate a file.
+	 * @param file the file to validate
+	 * @param extensions all extensions that should be considered valid
+	 * @return {@code true} if the file exists and its extension matches one of the given extensions
+	 */
 	public static boolean isValidFile(File file, String... extensions) {
 
 		if (!file.exists())
