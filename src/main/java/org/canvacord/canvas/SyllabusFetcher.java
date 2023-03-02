9754c7149b83b7c4d17da6d7581113c9af6ec443
Garrett <gnixsoftware@gmail.com>
2023-03-02 15:39:16 -0800

    Create SyllabusFetcher template

---
+++ src/main/java/org/canvacord/canvas/SyllabusFetcher.java
@@ -0,0 +1,12 @@
+package org.canvacord.canvas;
+
+import org.json.JSONObject;
+
+public class SyllabusFetcher {
+
+	public static SyllabusInfo fetchSyllabusForCourse(String courseID) {
+		// TODO
+		return new SyllabusInfo(new JSONObject());
+	}
+
+}
