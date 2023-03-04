ce7df1f72cc2e7824758de7468e5f6219a9f8048
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-03-04 15:04:07 -0800

    Quick changes for the work review

--- src/main/java/org/canvacord/scheduler/CanvasFetchScheduler.java
+++ src/main/java/org/canvacord/scheduler/CanvasFetchScheduler.java
@@ -10,6 +10,11 @@ public class CanvasFetchScheduler {
 	private static Scheduler fetchScheduler;
     private static final String GROUP_ID = "canvasFetch";
 
+    /**
+     * Initialize the Fetch scheduler. This prepares it for
+     * scheduling fetch jobs for Instances.
+     * @throws SchedulerException if something goes wrong getting the default scheduler
+     */
     public static void init() throws SchedulerException {
 
         fetchScheduler = StdSchedulerFactory.getDefaultScheduler();
@@ -18,13 +23,19 @@ public class CanvasFetchScheduler {
 
     }
 
+    /**
+     * Schedule all fetch jobs for the given Instance.
+     * @param instance if something goes wrong registering the jobs
+     */
     public static void scheduleInstance(Instance instance) throws SchedulerException {
 
+        // Build a job for fetching from Canvas
         JobDetail fetchJob = JobBuilder.newJob(CanvasFetchJob.class)
                 .withIdentity(instance.getInstanceID(), GROUP_ID)
                 .usingJobData("instanceID", instance.getInstanceID())
                 .build();
 
+        // Build a trigger for the job
         // TODO make this dynamic based on instance config
         Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity(instance.getInstanceID(), GROUP_ID)
@@ -41,6 +52,11 @@ public class CanvasFetchScheduler {
 
     }
 
+    /**
+     * Remove an Instance's fetch jobs from the scheduler.
+     * @param instance the Instance to remove
+     * @throws SchedulerException if something goes wrong removing the Instance's jobs
+     */
     public static void removeInstance(Instance instance) throws SchedulerException {
         JobKey key = new JobKey(instance.getInstanceID(), GROUP_ID);
         fetchScheduler.deleteJob(key);
--- src/main/java/org/canvacord/scheduler/NotificationScheduler.java
+++ src/main/java/org/canvacord/scheduler/NotificationScheduler.java
@@ -10,6 +10,11 @@ public class NotificationScheduler {
     private static Scheduler notifyScheduler;
     private static final String GROUP_ID = "discordNotify";
 
+    /**
+     * Initialize the Notification scheduler. This prepares it for
+     * scheduling notification jobs for Instances.
+     * @throws SchedulerException if something goes wrong getting the default scheduler
+     */
     public static void init() throws SchedulerException {
 
         notifyScheduler = StdSchedulerFactory.getDefaultScheduler();
@@ -18,34 +23,40 @@ public class NotificationScheduler {
 
     }
 
-    public static void scheduleInstance(Instance instance) {
+    /**
+     * Schedule all notification jobs for the given Instance.
+     * @param instance if something goes wrong registering the jobs
+     */
+    public static void scheduleInstance(Instance instance) throws SchedulerException {
 
-        try {
-            // TODO
-            JobDetail notifyJob = JobBuilder.newJob(NotificationJob.class)
-                    .withIdentity(instance.getInstanceID(), GROUP_ID)
-                    .usingJobData("serverID", instance.getServerID())
-                    .build();
+        // Build a job for sending notifications
+        JobDetail notifyJob = JobBuilder.newJob(NotificationJob.class)
+                .withIdentity(instance.getInstanceID(), GROUP_ID)
+                .usingJobData("serverID", instance.getServerID())
+                .build();
 
-            Trigger trigger = TriggerBuilder.newTrigger()
-                    .withIdentity(instance.getInstanceID(), GROUP_ID)
-                    .startNow()
-                    .withSchedule(
-                            SimpleScheduleBuilder.simpleSchedule()
-                                    .withIntervalInSeconds(15)
-                                    .repeatForever()
-                    ).build();
+        // Build a trigger for the job
+        // TODO make this dynamic based on instance config
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity(instance.getInstanceID(), GROUP_ID)
+                .startNow()
+                .withSchedule(
+                        SimpleScheduleBuilder.simpleSchedule()
+                                .withIntervalInSeconds(15)
+                                .repeatForever()
+                ).build();
 
-            notifyScheduler.scheduleJob(notifyJob, trigger);
+        notifyScheduler.scheduleJob(notifyJob, trigger);
 
-            System.out.println("Scheduled notifications for instance " + instance.getInstanceID());
-        }
-        catch (SchedulerException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Scheduled notifications for instance " + instance.getInstanceID());
 
     }
 
+    /**
+     * Remove an Instance's notification jobs from the scheduler.
+     * @param instance the Instance to remove
+     * @throws SchedulerException if something goes wrong removing the Instance's jobs
+     */
     public static void removeInstance(Instance instance) throws SchedulerException {
         JobKey key = new JobKey(instance.getInstanceID(), GROUP_ID);
         notifyScheduler.deleteJob(key);
