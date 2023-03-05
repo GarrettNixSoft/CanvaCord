f1463ce556bd75ac56b2ee1517158d541b4c29e1
royalty1free <54598612+royalty1free@users.noreply.github.com>
2023-03-04 19:40:56 -0800

    Online Textbook Download
    Added a download Textbook method in webscraper to allow users to download a textbook with a keyword search.

--- src/test/java/org/canvacord/ScraperTest.java
+++ src/test/java/org/canvacord/ScraperTest.java
@@ -3,6 +3,7 @@ package org.canvacord;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.canvacord.util.file.TextbookScraper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,7 +12,7 @@ import org.jsoup.select.Elements;
 public class ScraperTest {
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
+        /* Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a keyword to search for: ");
         String keyword = scanner.nextLine();
 
@@ -32,12 +33,14 @@ public class ScraperTest {
             System.out.println();
             //break;
         }
-        */
         bookRows.remove(0);
         bookRows.remove(0);
         bookRows.remove(0);
         System.out.println(bookRows.first().select("td").size());
         System.out.println(bookRows.first().select("td").get(1).text());
-        System.out.println(bookRows.first().select("td").get(9).select("a").attr("href"));
+        System.out.println(bookRows.first().select("td").get(9).select("a").attr("href")); */
+        //String url = TextbookScraper.getTextbookURL("calculus");
+        //System.out.println(url);
+        TextbookScraper.downloadTextbook("32202", "bio");
     }
 }
