f41f331d25ed59735b46d346de08aca1431bcaff
Garrett <gnixsoftware@gmail.com>
2023-03-07 16:13:52 -0800

    Fleshing out CanvaCordNotification

--- src/main/java/org/canvacord/discord/CanvaCordNotification.java
+++ src/main/java/org/canvacord/discord/CanvaCordNotification.java
@@ -1,5 +1,9 @@
 package org.canvacord.discord;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CanvaCordNotification {
@@ -7,9 +11,21 @@ public class CanvaCordNotification {
 	private long channelID;
 	private List<CanvaCordRole> rolesToPing;
 
-	public CanvaCordNotification(long channelID, List<CanvaCordRole> rolesToPing) {
+	private JSONObject schedule;
+	private String messageFormat;
+
+	public CanvaCordNotification(long channelID, List<CanvaCordRole> rolesToPing, JSONObject schedule, String messageFormat) {
 		this.channelID = channelID;
 		this.rolesToPing = rolesToPing;
+		this.schedule = schedule;
+		this.messageFormat = messageFormat;
+	}
+
+	public CanvaCordNotification(JSONObject configJSON) {
+		this.channelID = configJSON.getLong("channel_id");
+		this.schedule = configJSON.getJSONObject("schedule");
+		this.messageFormat = configJSON.getString("message_format");
+		readRolesFromJSON(configJSON.getJSONArray("roles"));
 	}
 
 	public long getChannelID() {
@@ -26,6 +42,29 @@ public class CanvaCordNotification {
 
 	public void setRolesToPing(List<CanvaCordRole> rolesToPing) {
 		this.rolesToPing = rolesToPing;
+	}
+
+	public JSONObject getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(JSONObject schedule) {
+		this.schedule = schedule;
+	}
+
+	public String getMessageFormat() {
+		return messageFormat;
+	}
+
+	public void setMessageFormat(String messageFormat) {
+		this.messageFormat = messageFormat;
+	}
+
+	private void readRolesFromJSON(JSONArray rolesArray) {
+		rolesToPing = new ArrayList<>();
+		for (Object obj : rolesArray.toList()) {
+			rolesToPing.add(new CanvaCordRole((JSONObject) obj));
+		}
 	}
 
 }
