6386191c9ea531026fbbf167f5fdb8178f572187
Francisco Rivera <frivera_123@live.com>
2023-03-09 15:35:56 -0800

    noob mistake

--- src/test/java/org/canvacord/DiscordBotTest.java
+++ src/test/java/org/canvacord/DiscordBotTest.java
@@ -20,22 +20,8 @@ public class DiscordBotTest {
         DiscordBot bot = DiscordBot.getBotInstance();
         bot.login();
 
-        bot.sendMessageToChannel("lmao", 1234567890);
-
-        // Potentially iterate through channel set?
-        //Channel channel = channels.iterator().next();
-
-
-        //Set<ChannelCategory> category = bot.getApi().getChannelCategories();
-
-        //Server server = bot.getApi().getServerById(1016848330992656415L).get();
-        //ServerTextChannel channel2 = new ServerTextChannelBuilder(server)
-        //        .setName("Assignments")
-        //        .create().join();
-
-        //Choose which channel to send the message to
-        //channel.sendMessage("Testing - Cisco, lmao");
-
+        bot.sendMessageToChannel("lmao", 1083512434653921352L);
+        
     }
 
 }
