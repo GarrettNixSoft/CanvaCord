7b8d26e3a9172d4fd04bb890f59795e4435eaf84
Garrett <gnixsoftware@gmail.com>
2023-03-09 16:32:50 -0800

    Added RemoteFileGetter.downloadAndSave

--- src/main/java/org/canvacord/util/net/RemoteFileGetter.java
+++ src/main/java/org/canvacord/util/net/RemoteFileGetter.java
@@ -2,10 +2,14 @@ package org.canvacord.util.net;
 
 import org.canvacord.util.input.UserInput;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class RemoteFileGetter {
 
@@ -33,6 +37,23 @@ public class RemoteFileGetter {
 			}
 			return null;
 		}
+
+	}
+
+	public static Optional<File> downloadAndSave(String url, Path saveTarget) {
+
+		try (FileOutputStream outputStream = new FileOutputStream(saveTarget.toFile())) {
+			InputStream downloadStream = getRemoteFileStream(url, false);
+			if (downloadStream == null)
+				return Optional.empty();
+			outputStream.write(downloadStream.readAllBytes());
+			return Optional.of(saveTarget.toFile());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Optional.empty();
+		}
+
 
 	}
 
