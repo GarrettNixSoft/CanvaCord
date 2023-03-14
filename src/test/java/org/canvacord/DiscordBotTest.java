9dd94629deae5ae49b55bda8821d8111b91c5257
Francisco Rivera <frivera_123@live.com>
2023-03-14 15:10:08 -0700

    simple slash command
    simple slash command test

--- src/test/java/org/canvacord/DiscordBotTest.java
+++ src/test/java/org/canvacord/DiscordBotTest.java
@@ -1,11 +1,19 @@
 package org.canvacord;
 
+import org.canvacord.discord.commands.Command;
+import org.canvacord.discord.commands.CommandBuilder;
 import org.canvacord.persist.ConfigManager;
+import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.*;
 import org.canvacord.discord.DiscordBot;
 import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.permission.PermissionType;
 import org.javacord.api.entity.server.Server;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Optional;
 import java.util.Set;
@@ -20,8 +28,25 @@ public class DiscordBotTest {
         DiscordBot bot = DiscordBot.getBotInstance();
         bot.login();
 
-        bot.sendMessageToChannel("lmao", 1083512434653921352L);
-        
+        //bot.sendMessageToChannel("lmao", 1234567890);
+
+        DiscordApi api = bot.getApi();
+
+        Server server = api.getServers().iterator().next();
+
+        //TEST
+        SlashCommand command = SlashCommand.with("ping", "Checks the functionality of this command")
+                .createForServer(server)
+                .join();
+        Set<SlashCommand> commands = api.getGlobalSlashCommands().join();
+
+        api.addSlashCommandCreateListener(event -> {
+            SlashCommandInteraction interaction = event.getSlashCommandInteraction();
+            if (interaction.getFullCommandName().equals("ping")) {
+                event.getInteraction().createImmediateResponder().setContent("pong").respond();
+            }
+        });
+
     }
 
 }
