6516f6652bb58717e4c0697d8091496048ae7532
Garrett <gnixsoftware@gmail.com>
2023-03-15 12:24:45 -0700

    Restructured how syllabus fetching will work

--- src/main/java/org/canvacord/canvas/CanvasApi.java
+++ src/main/java/org/canvacord/canvas/CanvasApi.java
@@ -70,20 +70,8 @@ public class CanvasApi {
 	}
 
 	// ******************************** CUSTOM SEARCHES ********************************
-	public SyllabusInfo findSyllabus(String courseID) {
-
-		// TODO:
-		// 0. Check if there's already a valid object for this course, and if so, return it immediately
-		// 1. Fetch the course from Canvas
-		// 2. Get its syllabus_body
-		// 3. If the syllabus_body is present and valid, search for a file link of type PDF or DOC (.doc, .docx, etc.)
-		// 4. If a file link is found, attempt to download the file
-		// 5. If the download succeeds, send the file through the appropriate parser for its file type
-		// 6. If the parser retrieves good data, write it to disk and return the object
-
-		// Fail state
-		return null;
-
+	public Optional<SyllabusInfo> findSyllabus(String courseID) {
+		return SyllabusFetcher.fetchSyllabusForCourse(courseID);
 	}
 
 
--- src/main/java/org/canvacord/canvas/SyllabusFetcher.java
+++ src/main/java/org/canvacord/canvas/SyllabusFetcher.java
@@ -2,11 +2,22 @@ package org.canvacord.canvas;
 
 import org.json.JSONObject;
 
+import java.util.Optional;
+
 public class SyllabusFetcher {
 
-	public static SyllabusInfo fetchSyllabusForCourse(String courseID) {
-		// TODO
-		return new SyllabusInfo(new JSONObject());
+	protected static Optional<SyllabusInfo> fetchSyllabusForCourse(String courseID) {
+
+		// TODO:
+		// 0. Check if there's already a valid object for this course, and if so, return it immediately
+		// 1. Fetch the course from Canvas
+		// 2. Get its syllabus_body
+		// 3. If the syllabus_body is present and valid, search for a file link of type PDF or DOC (.doc, .docx, etc.)
+		// 4. If a file link is found, attempt to download the file
+		// 5. If the download succeeds, send the file through the appropriate parser for its file type
+		// 6. If the parser retrieves good data, write it to disk and return the object
+
+		return Optional.empty();
 	}
 
 }
