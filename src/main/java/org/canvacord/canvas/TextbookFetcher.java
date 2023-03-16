bd690af82589e756412b883133e1f0b8881e1565
Garrett <gnixsoftware@gmail.com>
2023-03-16 11:03:49 -0700

    Added a class to contain textbook fetching code

---
+++ src/main/java/org/canvacord/canvas/TextbookFetcher.java
@@ -0,0 +1,18 @@
+package org.canvacord.canvas;
+
+import java.util.Optional;
+
+public class TextbookFetcher {
+
+	/**
+	 * Attempts to find a copy of a textbook online.
+	 * @param searchTerm the terms to use in searching for the textbook
+	 * @return an Optional type containing information about what was found,
+	 * or empty if no results were found.
+	 */
+	public static Optional<TextbookInfo> fetchTextbook(String searchTerm) {
+		// TODO
+		return Optional.empty();
+	}
+
+}
