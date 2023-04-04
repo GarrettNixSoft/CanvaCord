f381b1ca052a29c99d4055cf47579cdf1c5d17de
Garrett <gnixsoftware@gmail.com>
2023-04-04 15:28:04 -0700

    Reminder IDs are generated as UUIDs and stored as Strings

--- src/main/java/org/canvacord/reminder/Reminder.java
+++ src/main/java/org/canvacord/reminder/Reminder.java
@@ -3,11 +3,25 @@ package org.canvacord.reminder;
 import org.json.JSONObject;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record Reminder(long reminderID, long userID, long channelID, LocalDateTime triggerDate, String message) {
+public record Reminder(String reminderID, long userID, long channelID, LocalDateTime triggerDate, String message) {
+
+	/**
+	 * Builds a new Reminder using the given fields and with a random UUID.
+	 * @param userID the Discord ID of the target user
+	 * @param channelID the Discord ID of the target channel
+	 * @param triggerDate when the reminder should be sent
+	 * @param message the reminder message
+	 * @return a new Reminder object
+	 */
+	public static Reminder buildNew(long userID, long channelID, LocalDateTime triggerDate, String message) {
+		String reminderID = UUID.randomUUID().toString();
+		return new Reminder(reminderID, userID, channelID, triggerDate, message);
+	}
 
 	public static Reminder load(JSONObject reminderData) {
-		long reminderID = reminderData.getLong("id");
+		String reminderID = reminderData.getString("id");
 		long userID = reminderData.getLong("user_id");
 		long channelID = reminderData.getLong("channel_id");
 		LocalDateTime triggerDate = LocalDateTime.parse(reminderData.getString("trigger_date"));
