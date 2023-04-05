d3721169d1153beebb315169ea07bac310a1d8a5
Garrett <gnixsoftware@gmail.com>
2023-04-05 16:31:46 -0700

    Test implementation of the ReminderJob

--- src/main/java/org/canvacord/scheduler/job/ReminderJob.java
+++ src/main/java/org/canvacord/scheduler/job/ReminderJob.java
@@ -1,11 +1,19 @@
 package org.canvacord.scheduler.job;
 
+import org.canvacord.discord.DiscordBot;
 import org.canvacord.instance.Instance;
 import org.canvacord.reminder.Reminder;
 import org.canvacord.reminder.ReminderManager;
+import org.canvacord.util.time.CanvaCordTime;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
+import java.util.Optional;
 
 public class ReminderJob implements Job {
 
@@ -22,6 +30,34 @@ public class ReminderJob implements Job {
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		// TODO
+
+		// Grab the required objects for this job
+		instance = (Instance) context.get("instance");
+		reminder = (Reminder) context.get("reminder");
+
+		// Fetch the Discord API
+		DiscordApi discordApi = DiscordBot.getBotInstance().getApi();
+
+		// Make sure we can access the target channel
+		Optional<ServerTextChannel> targetChannelOpt = discordApi.getServerTextChannelById(reminder.channelID());
+		if (targetChannelOpt.isEmpty()) throw new JobExecutionException("Could not get channel!");
+
+		// Fetch the target user
+		User targetUser = discordApi.getUserById(reminder.userID()).join();
+
+		// Build an embed
+		EmbedBuilder embedBuilder = new EmbedBuilder();
+		embedBuilder.setTitle("Reminder");
+		embedBuilder.addField("Created At", CanvaCordTime.getFriendlyDateString(reminder.createdAt()));
+		embedBuilder.addField("Message", reminder.message());
+
+		// Send the embed to the user
+		targetUser.sendMessage(embedBuilder);
+
+		// TODO anything else?
+
+		// Remove this reminder from the instance data
+		ReminderManager.registerReminderSent(instance, reminder);
+
 	}
 }
