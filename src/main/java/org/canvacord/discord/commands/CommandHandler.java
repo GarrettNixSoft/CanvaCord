249f1fe99244d1b759ef20dd593df4e678c1759f
Garrett <gnixsoftware@gmail.com>
2023-04-05 16:55:39 -0700

    Changed map types for registered commands

--- src/main/java/org/canvacord/discord/commands/CommandHandler.java
+++ src/main/java/org/canvacord/discord/commands/CommandHandler.java
@@ -1,10 +1,10 @@
 package org.canvacord.discord.commands;
 
 import org.canvacord.exception.CanvaCordException;
+import org.canvacord.instance.InstanceManager;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.interaction.SlashCommand;
-import org.javacord.api.interaction.SlashCommandBuilder;
 import org.javacord.api.interaction.SlashCommandInteraction;
 
 import java.util.HashMap;
@@ -12,13 +12,14 @@ import java.util.Map;
 
 public class CommandHandler {
 
-	private static final Map<Long, Command> commands = new HashMap<>();
+	private static final Map<Long, Command> globalCommands = new HashMap<>();
+	private static final Map<String, Map<Long, Command>> serverCommands = new HashMap<>();
 
 	public static long registerCommandGlobal(Class<? extends Command> commandType, DiscordApi api) {
 		try {
 			Command commandObj = commandType.getConstructor().newInstance();
 			SlashCommand globalCommand = commandObj.getBuilder().createGlobal(api).join();
-			commands.put(globalCommand.getId(), commandObj);
+			globalCommands.put(globalCommand.getId(), commandObj);
 			return globalCommand.getId();
 		}
 		catch (Exception e) {
@@ -28,9 +29,10 @@ public class CommandHandler {
 
 	public static long registerCommandServer(Class<? extends Command> commandType, Server server) {
 		try {
+			String instanceID = InstanceManager.getInstanceByServerID(server.getId()).get().getInstanceID();
 			Command commandObj = commandType.getConstructor().newInstance();
 			SlashCommand serverCommand = commandObj.getBuilder().createForServer(server).join();
-			commands.put(serverCommand.getId(), commandObj);
+			serverCommands.get(instanceID).put(serverCommand.getId(), commandObj);
 			return serverCommand.getId();
 		}
 		catch (Exception e) {
@@ -39,9 +41,10 @@ public class CommandHandler {
 	}
 
 	public static void executeCommand(SlashCommandInteraction interaction) {
+		String instanceID = InstanceManager.getInstanceByServerID(interaction.getServer().get().getId()).get().getInstanceID();
 		long commandID = interaction.getCommandId();
-		if (commands.containsKey(commandID)) {
-			commands.get(commandID).execute(interaction);
+		if (serverCommands.get(instanceID).containsKey(commandID)) {
+			serverCommands.get(instanceID).get(commandID).execute(interaction);
 		}
 		else {
 			// Unregistered command!
