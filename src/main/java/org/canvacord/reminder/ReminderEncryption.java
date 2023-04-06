27b6c3ceb5e17980d7c191da5923364a4eb13a8b
Garrett <gnixsoftware@gmail.com>
2023-04-06 16:06:35 -0700

    Added Javadoc to ReminderEncryption

--- src/main/java/org/canvacord/reminder/ReminderEncryption.java
+++ src/main/java/org/canvacord/reminder/ReminderEncryption.java
@@ -1,25 +1,24 @@
 package org.canvacord.reminder;
 
-import org.canvacord.main.CanvaCord;
-
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Random;
 
+/**
+ * ReminderEncryption facilitates cryptographic obfuscation (NOT secure)
+ * of custom reminder messages.
+ */
 public class ReminderEncryption {
 
-	private static KeyGenerator keyGenerator;
+	// THe cipher object to use for actually performing cryptographic functions
 	private static final Cipher cipher;
 
 	static {
 		try {
-			keyGenerator = KeyGenerator.getInstance("AES");
 			cipher = Cipher.getInstance("AES");
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
 			e.printStackTrace();
@@ -27,36 +26,52 @@ public class ReminderEncryption {
 		}
 	}
 
+	/**
+	 * Encrypt the message stored in a Reminder.
+	 * @param reminder the reminder to encrypt
+	 * @return a copy of the reminder in which the message is replaced with an encrypted version
+	 */
 	public static Reminder encryptReminder(Reminder reminder) {
 		SecretKey secretKey = getKey(reminder.userID());
-		System.out.println("encryption key: " + secretKey.toString());
 		String encryptedMessage = encryptMessage(reminder.message(), secretKey);
 		return new Reminder(reminder.reminderID(), reminder.userID(), reminder.channelID(),
 							reminder.createdAt(), reminder.triggerDate(), true, encryptedMessage);
 	}
 
+	/**
+	 * Decrypt an encrypted message stored in a reminder.
+	 * @param reminder the reminder to decrypt
+	 * @return a copy of the reminder in which the message is replaced with the decrypted version
+	 */
 	public static Reminder decryptReminder(Reminder reminder) {
 		SecretKey secretKey = getKey(reminder.userID());
-		System.out.println("decryption key: " + secretKey.toString());
 		String decryptedMessage = decryptMessage(reminder.message(), secretKey);
 		return new Reminder(reminder.reminderID(), reminder.userID(), reminder.channelID(),
 							reminder.createdAt(), reminder.triggerDate(), true, decryptedMessage);
 	}
 
+	/**
+	 * Generate a {@code SecretKey} for encrypting/decrypting a message.
+	 * This key is generated deterministically using the user's
+	 * Discord ID, and is thus not truly secure -- a malicious
+	 * Owner running CanvaCord can still read the messages if they
+	 * try.
+	 * @param userID the ID of the user who created the reminder
+	 * @return a {@code SecretKey} generated using the user's ID as a seed
+	 */
 	private static SecretKey getKey(long userID) {
-		try {
-			keyGenerator = KeyGenerator.getInstance("AES");
-		}
-		catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			CanvaCord.explode();
-		}
 		Random random = new Random(userID);
 		byte[] bytes = new byte[32];
 		random.nextBytes(bytes);
 		return new SecretKeySpec(bytes, "AES");
 	}
 
+	/**
+	 * Encrypt a String using a {@code SecretKey}.
+	 * @param message the message String to encrypt
+	 * @param secretKey the key to use in the encryption process
+	 * @return the encrypted String
+	 */
 	private static String encryptMessage(String message, SecretKey secretKey) {
 		byte[] plainBytes = message.getBytes();
 		try {
@@ -71,6 +86,12 @@ public class ReminderEncryption {
 		}
 	}
 
+	/**
+	 * Decript a String using a {@code SecretKey}.
+	 * @param message the encrypted String to decrypt
+	 * @param secretKey the key to use in the decryption process
+	 * @return the decrypted String
+	 */
 	private static String decryptMessage(String message, SecretKey secretKey) {
 		Base64.Decoder decoder = Base64.getDecoder();
 		byte[] cipherBytes = decoder.decode(message.getBytes());
