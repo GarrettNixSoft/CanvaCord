6d35e3181004ad0c3512904f2f7db8f862c841fe
Garrett <gnixsoftware@gmail.com>
2023-04-11 13:36:52 -0700

    Template for sending assignment notifications

---
+++ src/main/java/org/canvacord/discord/notification/AssignmentNotifier.java
@@ -0,0 +1,22 @@
+package org.canvacord.discord.notification;
+
+import edu.ksu.canvas.model.assignment.Assignment;
+import org.canvacord.entity.CanvaCordNotification;
+
+import java.util.List;
+
+public class AssignmentNotifier {
+
+	public static void notifyNewAssignments(CanvaCordNotification notificationConfig, List<Assignment> assignments) {
+		// TODO
+	}
+
+	public static void notifyDueDateChanged(CanvaCordNotification notificationConfig, Assignment assignment) {
+		// TODO
+	}
+
+	public static void notifyDueDateApproaching(CanvaCordNotification notificationConfig, Assignment assignment) {
+		// TODO
+	}
+
+}
