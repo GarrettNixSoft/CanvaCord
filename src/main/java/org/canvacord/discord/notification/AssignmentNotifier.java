82291b1bbc1c1e3a596a83438d98a90d786f8e0b
Garrett <gnixsoftware@gmail.com>
2023-04-11 13:41:15 -0700

    Changed return type and added Javadoc descriptions for AssignmentNotifier methods

--- src/main/java/org/canvacord/discord/notification/AssignmentNotifier.java
+++ src/main/java/org/canvacord/discord/notification/AssignmentNotifier.java
@@ -7,16 +7,43 @@ import java.util.List;
 
 public class AssignmentNotifier {
 
-	public static void notifyNewAssignments(CanvaCordNotification notificationConfig, List<Assignment> assignments) {
+	/**
+	 * Send notifications to users about newly posted assignments.
+	 * @param notificationConfig a notification configured by the Owner which specifies
+	 *                           the target channel, roles to ping, and message format
+	 *                           to be used
+	 * @param assignments a list of assignments found on Canvas
+	 * @return {@code true} if all notifications are sent successfully, or false otherwise
+	 */
+	public static boolean notifyNewAssignments(CanvaCordNotification notificationConfig, List<Assignment> assignments) {
 		// TODO
+		return false;
 	}
 
-	public static void notifyDueDateChanged(CanvaCordNotification notificationConfig, Assignment assignment) {
+	/**
+	 * Send notifications to users about a changed due date.
+	 * @param notificationConfig a notification configured by the Owner which specifies
+	 *                           the target channel, roles to ping, and message format
+	 *                           to be used
+	 * @param assignment the assignment whose due date was changed
+	 * @return {@code true} if the notification is sent successfully, or false otherwise
+	 */
+	public static boolean notifyDueDateChanged(CanvaCordNotification notificationConfig, Assignment assignment) {
 		// TODO
+		return false;
 	}
 
-	public static void notifyDueDateApproaching(CanvaCordNotification notificationConfig, Assignment assignment) {
+	/**
+	 * Send notifications to users about an approaching assignment due date.
+	 * @param notificationConfig a notification configured by the Owner which specifies
+	 *                           the target channel, roles to ping, and message format
+	 *                           to be used
+	 * @param assignment the assignment that is due soon
+	 * @return {@code true} if the notification is sent successfully, or false otherwise
+	 */
+	public static boolean notifyDueDateApproaching(CanvaCordNotification notificationConfig, Assignment assignment) {
 		// TODO
+		return false;
 	}
 
 }
