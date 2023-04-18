8b2e129153ebfcf8e8027715c7a96c0c6d32a837
Garrett <gnixsoftware@gmail.com>
2023-04-18 11:39:26 -0700

    Added first() and second() alias methods to Pair

--- src/main/java/org/canvacord/util/data/Pair.java
+++ src/main/java/org/canvacord/util/data/Pair.java
@@ -1,3 +1,13 @@
 package org.canvacord.util.data;
 
-public record Pair<T1, T2>(T1 data1, T2 data2) {}
+public record Pair<T1, T2>(T1 data1, T2 data2) {
+
+	public T1 first() {
+		return data1;
+	}
+
+	public T2 second() {
+		return data2;
+	}
+
+}
