a312a8110a1af107c63887457755743457c27e7f
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-04-22 22:59:29 -0700

    Changed signature for CommandBuilder.buildCommandsForServer to use command classes

--- src/main/java/org/canvacord/discord/commands/CommandBuilder.java
+++ src/main/java/org/canvacord/discord/commands/CommandBuilder.java
@@ -13,9 +13,9 @@ public class CommandBuilder {
 	 * @param server The server that commands are being built for.
 	 * @param serverActiveCommands A set of owner chosen commands that determines what's available.
 	 * */
-	public static void buildCommandsForServer(Server server, Set<Command> serverActiveCommands) {
-		for (Command command:serverActiveCommands){
-			registerCommandServer(command.getClass(), server);
+	public static void buildCommandsForServer(Server server, Set<Class<? extends Command>> serverActiveCommands) {
+		for (Class<? extends Command> command : serverActiveCommands){
+			registerCommandServer(command, server);
 		}
 		// Help command is registered last, so it can populate its autocomplete choices
 		registerCommandServer(HelpCommand.class, server);
