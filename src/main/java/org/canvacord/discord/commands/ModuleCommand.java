12f32272f74a639769fde0365f6f9f67375a670b
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-04-23 00:10:10 -0700

    Tabs

--- src/main/java/org/canvacord/discord/commands/ModuleCommand.java
+++ src/main/java/org/canvacord/discord/commands/ModuleCommand.java
@@ -25,86 +25,86 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ModuleCommand extends Command implements ButtonClickListener {
-    private int assignmentsPerPage = 3; //arbitrary number
-    private int currentPage;
-    private Instance instance;
-    JSONArray fetchedModules = new JSONArray();
+	private int assignmentsPerPage = 3; //arbitrary number
+	private int currentPage;
+	private Instance instance;
+	JSONArray fetchedModules = new JSONArray();
 
 
-    @Override
-    public String getDescription() {
-        return "see course module list for specified class";
-    }
+	@Override
+	public String getDescription() {
+		return "see course module list for specified class";
+	}
 
-    @Override
-    public String getShortDescription() {
-        return "see course module list";
-    }
+	@Override
+	public String getShortDescription() {
+		return "see course module list";
+	}
 
-    @Override
-    public String getName() {
-        return "modulelist";
-    }
+	@Override
+	public String getName() {
+		return "modulelist";
+	}
 
-    @Override
-    public void execute(SlashCommandInteraction interaction) {
+	@Override
+	public void execute(SlashCommandInteraction interaction) {
 
-        // get instance, server, course id
-        Server server = interaction.getServer().orElseThrow(CanvaCordException::new);
-        instance = InstanceManager.getInstanceByServerID(server.getId()).orElseThrow(CanvaCordException::new);
-        //CompletableFuture<InteractionOriginalResponseUpdater> response;
-        DiscordApi api = interaction.getApi();
+		// get instance, server, course id
+		Server server = interaction.getServer().orElseThrow(CanvaCordException::new);
+		instance = InstanceManager.getInstanceByServerID(server.getId()).orElseThrow(CanvaCordException::new);
+		//CompletableFuture<InteractionOriginalResponseUpdater> response;
+		DiscordApi api = interaction.getApi();
 
-        // grab the optional boolean for refreshing
-        boolean refresh;
-        Optional<SlashCommandInteractionOption> refreshOption = interaction.getOptionByIndex(0);
-        if (refreshOption.isPresent()) {
-            Optional<Boolean> refreshValue = refreshOption.get().getBooleanValue();
-            refresh = refreshValue.orElse(false);
-        } else {
-            refresh = false;
-        }
+		// grab the optional boolean for refreshing
+		boolean refresh;
+		Optional<SlashCommandInteractionOption> refreshOption = interaction.getOptionByIndex(0);
+		if (refreshOption.isPresent()) {
+			Optional<Boolean> refreshValue = refreshOption.get().getBooleanValue();
+			refresh = refreshValue.orElse(false);
+		} else {
+			refresh = false;
+		}
 
-        interaction.respondLater(true).thenAccept(interactionOriginalResponseUpdater -> {
+		interaction.respondLater(true).thenAccept(interactionOriginalResponseUpdater -> {
 
-            // grab the API in case it's needed
-            CanvasApi canvasApi = CanvasApi.getInstance();
+			// grab the API in case it's needed
+			CanvasApi canvasApi = CanvasApi.getInstance();
 
-            // Check the cache for modules
-            JSONArray cachedModules = CacheManager.getCachedModuleEntities(instance.getCourseID());
+			// Check the cache for modules
+			JSONArray cachedModules = CacheManager.getCachedModuleEntities(instance.getCourseID());
 
-            // If the user asked for a refresh, or the cache has no modules, fetch them; otherwise return what's in the cache
-            fetchedModules = refresh || cachedModules.isEmpty() ? canvasApi.getAllModuleFiles(instance.getCourseID()) : cachedModules;
+			// If the user asked for a refresh, or the cache has no modules, fetch them; otherwise return what's in the cache
+			fetchedModules = refresh || cachedModules.isEmpty() ? canvasApi.getAllModuleFiles(instance.getCourseID()) : cachedModules;
 
-            if (fetchedModules.isEmpty()) {
-                interactionOriginalResponseUpdater.setFlags(MessageFlag.EPHEMERAL).setContent("An error occurred when fetching modules.").update();
-                return;
-            }
+			if (fetchedModules.isEmpty()) {
+				interactionOriginalResponseUpdater.setFlags(MessageFlag.EPHEMERAL).setContent("An error occurred when fetching modules.").update();
+				return;
+			}
 
-            // get the minimum between the default value or the num of fetched assignments
-            assignmentsPerPage = Math.min(assignmentsPerPage,fetchedModules.length());
-            for (EmbedBuilder embed : assignmentEmbeds().subList(0, assignmentsPerPage)) {
-                interactionOriginalResponseUpdater.addEmbed(embed).update();
-            }
+			// get the minimum between the default value or the num of fetched assignments
+			assignmentsPerPage = Math.min(assignmentsPerPage,fetchedModules.length());
+			for (EmbedBuilder embed : assignmentEmbeds().subList(0, assignmentsPerPage)) {
+				interactionOriginalResponseUpdater.addEmbed(embed).update();
+			}
 
-            if (fetchedModules.length()>assignmentsPerPage) {
-                interactionOriginalResponseUpdater
-                        .addComponents(ActionRow.of(Button.danger("forward", "▶")))
-                        .update().thenAccept(originalResponse -> {
-                            api.addButtonClickListener(this).removeAfter(10, TimeUnit.MINUTES);
-                        });
-            }
+			if (fetchedModules.length()>assignmentsPerPage) {
+				interactionOriginalResponseUpdater
+						.addComponents(ActionRow.of(Button.danger("forward", "▶")))
+						.update().thenAccept(originalResponse -> {
+							api.addButtonClickListener(this).removeAfter(10, TimeUnit.MINUTES);
+						});
+			}
 
-        });
-    }
-    private List<EmbedBuilder> assignmentEmbeds(){
-        List <EmbedBuilder> moduleEmbeds = new ArrayList<>();
-        for (int i=0; i < fetchedModules.length(); i++) {
-            System.out.println(fetchedModules.getJSONObject(i).get("title").toString());
-            EmbedBuilder assignmentEmbed = new EmbedBuilder()
-                    .setTitle(fetchedModules.getJSONObject(i).get("title").toString())
-                    .setUrl(fetchedModules.getJSONObject(i).get("url").toString())
-                    .setColor(Color.RED);
+		});
+	}
+	private List<EmbedBuilder> assignmentEmbeds(){
+		List <EmbedBuilder> moduleEmbeds = new ArrayList<>();
+		for (int i=0; i < fetchedModules.length(); i++) {
+			System.out.println(fetchedModules.getJSONObject(i).get("title").toString());
+			EmbedBuilder assignmentEmbed = new EmbedBuilder()
+					.setTitle(fetchedModules.getJSONObject(i).get("title").toString())
+					.setUrl(fetchedModules.getJSONObject(i).get("url").toString())
+					.setColor(Color.RED);
             /*
             if (assignment.getDescription()!= null){
                 assignmentEmbed.setDescription(assignment.getDescription());
@@ -116,57 +116,57 @@ public class ModuleCommand extends Command implements ButtonClickListener {
                 assignmentEmbed.addField("Due",assignment.getDueAt().toString());
             }
             */
-            moduleEmbeds.add(assignmentEmbed);
-        }
-        return moduleEmbeds;
-    }
+			moduleEmbeds.add(assignmentEmbed);
+		}
+		return moduleEmbeds;
+	}
 
 
-    @Override
-    public SlashCommandBuilder getBuilder(Instance instance) {
-        return SlashCommand.with(getName(),getShortDescription(),
-                List.of(SlashCommandOption.create(SlashCommandOptionType.BOOLEAN, "refresh", "Force CanvaCord to refresh the modules list from Canvas.", false)));
-    }
+	@Override
+	public SlashCommandBuilder getBuilder(Instance instance) {
+		return SlashCommand.with(getName(),getShortDescription(),
+				List.of(SlashCommandOption.create(SlashCommandOptionType.BOOLEAN, "refresh", "Force CanvaCord to refresh the modules list from Canvas.", false)));
+	}
 
-    @Override
-    public void onButtonClick(ButtonClickEvent buttonClickEvent) {
+	@Override
+	public void onButtonClick(ButtonClickEvent buttonClickEvent) {
 
-        ButtonInteraction interaction = buttonClickEvent.getButtonInteraction();
-        ComponentInteractionOriginalMessageUpdater response = interaction.createOriginalMessageUpdater();
-        String buttonID = interaction.getCustomId();
-        List<LowLevelComponent> buttons = new ArrayList<>();
+		ButtonInteraction interaction = buttonClickEvent.getButtonInteraction();
+		ComponentInteractionOriginalMessageUpdater response = interaction.createOriginalMessageUpdater();
+		String buttonID = interaction.getCustomId();
+		List<LowLevelComponent> buttons = new ArrayList<>();
 
-        int pageStart = currentPage * assignmentsPerPage;
-        int pageEnd;
+		int pageStart = currentPage * assignmentsPerPage;
+		int pageEnd;
 
-        if (buttonID.equals("forward") && fetchedModules.length()-(pageStart+assignmentsPerPage)>0){
-            currentPage+=1;
-            buttons.add(Button.danger("back","◀"));
+		if (buttonID.equals("forward") && fetchedModules.length()-(pageStart+assignmentsPerPage)>0){
+			currentPage+=1;
+			buttons.add(Button.danger("back","◀"));
 
-            if(fetchedModules.length()-((currentPage * assignmentsPerPage)+assignmentsPerPage)>0){ //ugly yo
-                buttons.add(Button.danger("forward", "▶"));
-            }
-        }
-        else if (buttonID.equals("back") && currentPage!=0) {
-            currentPage -= 1;
+			if(fetchedModules.length()-((currentPage * assignmentsPerPage)+assignmentsPerPage)>0){ //ugly yo
+				buttons.add(Button.danger("forward", "▶"));
+			}
+		}
+		else if (buttonID.equals("back") && currentPage!=0) {
+			currentPage -= 1;
 
-            if(currentPage != 0){
-                buttons.add(Button.danger("back","◀"));
-            }
+			if(currentPage != 0){
+				buttons.add(Button.danger("back","◀"));
+			}
 
-            // put after the if statement to preserve button order
-            buttons.add(Button.danger("forward", "▶"));
-        }
+			// put after the if statement to preserve button order
+			buttons.add(Button.danger("forward", "▶"));
+		}
 
-        pageStart = currentPage * assignmentsPerPage;
-        pageEnd = Math.min(pageStart + assignmentsPerPage, fetchedModules.length());
+		pageStart = currentPage * assignmentsPerPage;
+		pageEnd = Math.min(pageStart + assignmentsPerPage, fetchedModules.length());
 
 
-        response.removeAllEmbeds();
-        for (EmbedBuilder embed : assignmentEmbeds().subList(pageStart, pageEnd)) {
-            response.addEmbed(embed);
-        }
+		response.removeAllEmbeds();
+		for (EmbedBuilder embed : assignmentEmbeds().subList(pageStart, pageEnd)) {
+			response.addEmbed(embed);
+		}
 
-        response.addComponents(ActionRow.of(buttons)).update();
-    }
+		response.addComponents(ActionRow.of(buttons)).update();
+	}
 }
