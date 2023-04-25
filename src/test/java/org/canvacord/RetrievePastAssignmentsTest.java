3ffc37b4b126152a0078bf76fb315894873c3cf7
royalty1free <54598612+royalty1free@users.noreply.github.com>
2023-04-24 21:34:16 -0700

    RetrievePastAssignmentsTest Finalization
    -Displays all past assignments
    -If ran again, will only output a message that has new past assignments

--- src/test/java/org/canvacord/RetrievePastAssignmentsTest.java
+++ src/test/java/org/canvacord/RetrievePastAssignmentsTest.java
@@ -1,5 +1,6 @@
 package org.canvacord;
 
+import edu.ksu.canvas.model.Conversation;
 import edu.ksu.canvas.model.assignment.Assignment;
 import edu.ksu.canvas.requestOptions.ListCourseAssignmentsOptions;
 import org.canvacord.canvas.CanvasApi;
@@ -8,18 +9,20 @@ import org.canvacord.persist.ConfigManager;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.channel.*;
+import org.javacord.api.entity.message.*;
 import org.javacord.api.entity.server.Server;
 
 import java.io.IOException;
-import java.nio.channels.Channels;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class RetrievePastAssignmentsTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ConfigManager.loadConfig();
         DiscordBot bot = DiscordBot.getBotInstance();
         bot.login();
@@ -41,16 +44,35 @@ public class RetrievePastAssignmentsTest {
 
         // Retrieve assignment list from Canvas API
         List<Assignment> assignments = canvasApi.getAssignmentsOptions("32202", "", ListCourseAssignmentsOptions.Bucket.PAST);
-        TextChannel testing = api.getTextChannelById(1100235269334110278L).get();
-        //bot.sendMessageToChannel("lmao", 1100235269334110278L);
-        Set<ServerTextChannel> channels = api.getServerTextChannelsByName("past-assignments");
-        ArrayList<ServerTextChannel> textChannels = new ArrayList<ServerTextChannel>(channels);
+        Set<TextChannel> channels = api.getTextChannelsByName("past-assignments");
+        ArrayList<TextChannel> textChannels = new ArrayList<TextChannel>(channels);
         if(!textChannels.isEmpty())
-            bot.sendMessageToChannel("Hi!", (long) textChannels.get(0).getId());
+            //bot.sendMessageToChannel("hi", (long) textChannels.get(0).getId());
+        //bot.sendMessageToChannel("hi", 1100235269334110278L);
         System.out.println(textChannels.get(0).getId());
-        for(int i = 0; i < assignments.size(); i++) {
-            System.out.println(assignments.get(i).getName());
+        MessageSet messages = textChannels.get(0).getMessages(100).join();
+        ArrayList<Message> messageList = new ArrayList<Message>(messages);
+        ArrayList<String> messageContentList = new ArrayList<String>();
+        for(Message message : messageList)
+        {
+            String[] temp = message.getContent().split("\n");
+            messageContentList.addAll(Arrays.asList(temp));
         }
+//        System.out.println("This statement is: " + messageContentList.contains("this is a message"));
+//        System.out.println("message");
+        /*for(Message message : messageList) {
+            System.out.println(message.getContent());
+        }*/
+        String combinedString = "";
+        for (Assignment assignment : assignments) {
+            if(!messageContentList.contains(assignment.getName())) {
+                //bot.sendMessageToChannel(assignment.getName(), (long) textChannels.get(0).getId());
+                //Thread.sleep(1000);
+                combinedString += assignment.getName() + "\n";
+            }
+        }
+        bot.sendMessageToChannel(combinedString, (long) textChannels.get(0).getId());
+        Thread.sleep(100);
         bot.disconnect();
     }
 }
