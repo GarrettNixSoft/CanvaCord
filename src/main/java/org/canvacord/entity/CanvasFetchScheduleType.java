d1bcf0dacf6aec7618926aa603be27cf5fc8ed76
Garrett <gnixsoftware@gmail.com>
2023-04-26 14:00:09 -0700

    Renamed CanvasFetchScheduleType.CUSTOM to CRON

--- src/main/java/org/canvacord/entity/CanvasFetchScheduleType.java
+++ src/main/java/org/canvacord/entity/CanvasFetchScheduleType.java
@@ -6,10 +6,10 @@ public class CanvasFetchScheduleType {
 	public static final String HOURLY = "Hourly";
 	public static final String DAILY = "Daily";
 	public static final String WEEKLY = "Weekly";
-	public static final String CUSTOM = "Custom";
+	public static final String CRON = "Cron";
 
 	public static final String[] TYPES = {
-			FREQUENT, HOURLY, DAILY, WEEKLY, CUSTOM
+			FREQUENT, HOURLY, DAILY, WEEKLY, CRON
 	};
 
 }
