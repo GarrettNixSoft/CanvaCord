27fa510c11e3cfe2959848965f8d7f9b7f575fc7
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-04-26 23:21:08 -0700

    Fixed color decoding

--- src/main/java/org/canvacord/util/CanvaCordColors.java
+++ src/main/java/org/canvacord/util/CanvaCordColors.java
@@ -10,10 +10,10 @@ public class CanvaCordColors {
 			throw new IllegalStateException("Color string must be 8 characters long");
 
 		int r, g, b, a;
-		r = Integer.parseInt(colorStr.substring(0, 2), 16);
-		g = Integer.parseInt(colorStr.substring(2, 4), 16);
-		b = Integer.parseInt(colorStr.substring(4, 6), 16);
-		a = Integer.parseInt(colorStr.substring(6, 8), 16);
+		a = Integer.parseInt(colorStr.substring(0, 2), 16);
+		r = Integer.parseInt(colorStr.substring(2, 4), 16);
+		g = Integer.parseInt(colorStr.substring(4, 6), 16);
+		b = Integer.parseInt(colorStr.substring(6, 8), 16);
 
 		return new Color(r, g, b, a);
 
