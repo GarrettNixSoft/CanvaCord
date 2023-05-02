fab14ffd1640e55d5038272403be466d30475feb
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-05-01 21:24:30 -0700

    Created classes with method endpoints for initialization

---
+++ src/main/java/org/canvacord/discord/initialize/CommandRegistration.java
@@ -0,0 +1,11 @@
+package org.canvacord.discord.initialize;
+
+import org.canvacord.instance.Instance;
+
+public class CommandRegistration {
+
+	public static void registerCommands(Instance instance) {
+		// TODO
+	}
+
+}
---
+++ src/main/java/org/canvacord/discord/initialize/RoleRegistration.java
@@ -0,0 +1,11 @@
+package org.canvacord.discord.initialize;
+
+import org.canvacord.instance.Instance;
+
+public class RoleRegistration {
+
+	public static void registerRoles(Instance instance) {
+		// TODO
+	}
+
+}
