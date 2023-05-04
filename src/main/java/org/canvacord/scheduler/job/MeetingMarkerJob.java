80ee4a39fa0f99e48ac85c4c4fc5c0b0fb1e2fb5
Garrett <gnixsoftware@gmail.com>
2023-05-03 18:31:43 -0700

    [WIP] Implementing meeting markers

--- src/main/java/org/canvacord/scheduler/job/MeetingMarkerJob.java
+++ src/main/java/org/canvacord/scheduler/job/MeetingMarkerJob.java
@@ -1,6 +1,8 @@
 package org.canvacord.scheduler.job;
 
+import org.canvacord.instance.Instance;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
@@ -8,6 +10,12 @@ public class MeetingMarkerJob implements Job {
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
+
+		JobDataMap dataMap = context.getMergedJobDataMap();
+		Instance instance = (Instance) dataMap.get("instance");
+
+
+
 		// TODO
 	}
 
