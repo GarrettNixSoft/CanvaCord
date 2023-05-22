367c80ffe962fcd60a85084c6ff61b219a67bef8
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-05-22 15:56:31 -0700

    Add logo, use it as default instance icon

--- src/main/java/org/canvacord/util/Globals.java
+++ src/main/java/org/canvacord/util/Globals.java
@@ -2,7 +2,7 @@ package org.canvacord.util;
 
 public class Globals {
 
-	public static final String DEFAULT_INSTANCE_ICON_PATH = "default_instance_icon.png";
+	public static final String DEFAULT_INSTANCE_ICON_PATH = "logo.png";
 
 	public static String EDIT_INSTANCE_ID = "";
 
